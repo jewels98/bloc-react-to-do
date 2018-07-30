b7b763b2b8ea6b94174b72ab3108d12066b5694a
jewels98 <mrwright0825@gmail.com>
2018-07-30 15:01:10 -0400

    Checkpoint-8: Complete

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -4,7 +4,7 @@ class ToDo extends Component {
     render() {
         return (
             <li>
-                < input type = "checkbox" checked = { this.props.isComplete }/> 
+                < input type = "checkbox" checked = { this.props.isComplete } onChange={ this.props.toggleComplete } /> 
                 <span> { this.props.description } </span>
             </li>
         );
